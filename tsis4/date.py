aa7f1c6efe53b8037a640412f77b2e32a949220b
baimen0v <112787953+baimen0v@users.noreply.github.com>
2023-02-22 23:39:06 +0600

    tsis4

---
+++ tsis4/date.py
@@ -0,0 +1,19 @@
+from datetime import datetime as date
+from datetime import timedelta as delta
+#1
+vremya= date.now()
+new_date= vremya-delta(days=5)
+print(new_date)
+#2
+yesterday= vremya-delta(days=1)
+tomorrow= vremya+delta(days=1)
+print(yesterday)
+print(vremya)
+print(tomorrow)
+#3
+print(vremya.replace(microsecond=0))
+#4
+date1=date(2022,2,14,12,0,0)
+date2=date(1999,2,14,0,10,0)
+diff=(date1-date2).total_seconds()
+print(diff)
